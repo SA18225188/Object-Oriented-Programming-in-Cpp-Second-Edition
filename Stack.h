4026050fd33d61c21bf8f8a729f9e3c240a56429
SA18225188 <1582831466@qq.com>
2018-10-06 22:53:45 +0800

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -1,17 +1,23 @@
-class Stack {
-public: 
-	enum { MAX_STACK = 5 };
-	
-	Stack() { init(); } 
-	void init() { top = -1; }
-	void push( int n );
-	int pop();
-	bool isEmpty() { return top < 0; }
-	bool isFull() { return top >= MAX_STACK-1; }
-	void dump();
-private:
-	int top;
-	int arr[ MAX_STACK ];
-	int dummyVal;
-	void errMsg( const char* msg ) const; 
-}; 
+class Stack{
+	public:
+		enum {MAX_STACK = 5};//const int MAX_STACK = 5;can't work
+		Stack(){
+			init();
+		}
+		void init(){
+		top=-1;
+		}
+		void push(int n);
+		int pop();
+		bool isEmpty(){
+			return top<0;
+		}
+		bool isFull(){
+			return top>=MAX_STACK-1;
+		}
+		void print();
+	private:
+		int top;
+		int arr[MAX_STACK];
+		 
+};
